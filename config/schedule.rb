1aa9492067bb7972dff1e4233067a2259e91d6bd
Evgeniy <agasyanc@gmail.com>
2024-09-13 16:32:45 +0400

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,7 +16,7 @@ env :PATH, ENV['PATH'] + ':/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin'
 #   rake "some:great:rake:task"
 # end
 #
-every 30.minutes do
+every 5.minutes do
   runner "Switch.turn_off"
 end
 
